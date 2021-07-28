668598f602c96a9019db3d8f72c7a4db3cffaea2
yashank <yaashank.mittal@gmail.com>
2021-07-28 13:15:12 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+
+const app = express();
+dotenv.config();   
+
+
+app.use(express.json({limit: "30mb",extended: true}));
+app.use(express.urlencoded({limit: "30mb",extended: true}));
+app.use(cors());
+
+
+app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
+
+app.get('/',(req,res) => {
+    res.send("APP IS RUNNING");
+})
+
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false})
+    .then(()=> app.listen(PORT,()=>console.log(`Server is running on port: ${PORT}`)))
+    .catch((e)=>console.log(`${e} did not connect`));
+
+    
