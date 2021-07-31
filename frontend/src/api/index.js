ab14411fc8f40d26099c8ce6c29472a1405d9749
yashank <yaashank.mittal@gmail.com>
2021-07-31 15:16:30 +0530

    hello

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8080' });
+const API = axios.create({ baseURL: 'https://serene-fortress-29319.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
